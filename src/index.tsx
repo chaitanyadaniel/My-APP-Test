8f963d4d1efe1b2afb852ab501e841c7cb4ce2b0
chaitanyadaniel <pchaitanyadaniel@gmail.com>
2026-07-22 17:07:15 +0000

    Welcome component done

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Welcome!</h1>
+      <p>This is a simple welcome component.</p>
+    </div>
+  );
+};
+
+export default Welcome;
